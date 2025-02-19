37d68e5043e0b3e807bbcc7554ee5565a27f4bda
Bahram Shamshiri <jowharshamshiri@gmail.com>
2025-02-19 18:11:04 +0100

    refactor: update import paths for parser modules; add parser build script to package.json; introduce new parser type definitions and styles

--- src/parser.d.ts
+++ src/parser.d.ts
@@ -201,9 +201,9 @@ export declare const parse: typeof ParseFunction;
 declare function ParseFunction<Options extends ParseOptions<"Start">>(
   input: string,
   options?: Options,
-): RootNode;
+): any;
 
 declare function ParseFunction<Options extends ParseOptions<StartRuleNames>>(
   input: string,
   options?: Options,
-): RootNode;
+): any;
